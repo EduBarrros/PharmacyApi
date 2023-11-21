a96ae2c1c49277513b9c4a766f1473ae7f34e334
EduBarrros <eduardodeoliveirabarros@gmail.com>
2023-11-21 15:06:28 -0300

    Build: Project base added

---
+++ src/test/java/com/pharmacy/PharmacyApi/PharmacyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacy.PharmacyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
