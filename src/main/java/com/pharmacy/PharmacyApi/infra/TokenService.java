161f5b596499943dc03960460c6fd241d2dcee6a
EduBarrros <eduardodeoliveirabarros@gmail.com>
2023-12-22 16:46:11 -0300

    Feat: token validation added

--- src/main/java/com/pharmacy/PharmacyApi/infra/TokenService.java
+++ src/main/java/com/pharmacy/PharmacyApi/infra/TokenService.java
@@ -3,6 +3,7 @@ package com.pharmacy.PharmacyApi.infra;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.pharmacy.PharmacyApi.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,19 @@ public class TokenService {
             return JWT.create().withIssuer("PharmacyApi").withSubject(user.getUsername()).withClaim("id", user.getId()).withExpiresAt(ExpirationDate()).sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    public String getSubject(String TokenJWT){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("PharmacyApi")
+                    .build()
+                    .verify(TokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado.", exception);
         }
     }
 
